94b8f7610dd8d074d2cd162c13857906f1a09ad3
Nupur <nupurdhuri445@gmail.com>
2025-04-20 06:48:02 +0530

    Fix: changed models folder to models.py file

--- models/booking.py
+++
@@ -1,6 +0,0 @@
-class TicketBooking:
-    def __init__(self, name, email, destination, date):
-        self.name = name
-        self.email = email
-        self.destination = destination
-        self.date = date
